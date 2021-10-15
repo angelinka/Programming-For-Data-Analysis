512d0bf9ffc0828bb70e94f31197673eab28f611
Hendrie Test <hendrietest@Hendries-MacBook-Air.local>
2021-10-15 15:57:12 +0100

    Playing with plots

---
+++ 2dplot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(0.0, 10.0, 0.01)
+y =  3.0 * x + 1.0
+noise =  np.random.normal(0.0, 1.0, len(x))
+
+plt.plot(x,y + noise, 'r.', label='Actual')
+plt.plot(x,y, 'b-', label='Model')
+
+#plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'b.')
+#plt.ylabel('some numbers')
+plt.title('Simple plot')
+plt.xlabel('Weight')
+plt.ylabel('Mass')
+plt.legend()
+plt.show()
+
